d3fbcb234dc98bb1c089e74e9e95392e8d0d2ebc
Gaurav Bajaj <letsdecode@gmail.com>
2025-06-07 17:31:49 -0700

    Adds Api Key through interceptor in OkHttp for centralization.

--- app/src/main/java/com/gauravbajaj/newsapp/data/api/NetworkService.kt
+++ app/src/main/java/com/gauravbajaj/newsapp/data/api/NetworkService.kt
@@ -2,27 +2,22 @@ package com.gauravbajaj.newsapp.data.api
 
 import com.gauravbajaj.newsapp.data.model.SourcesResponse
 import com.gauravbajaj.newsapp.data.model.TopHeadlinesResponse
-import com.gauravbajaj.newsapp.utils.AppConstant.API_KEY
 import retrofit2.http.GET
-import retrofit2.http.Headers
 import retrofit2.http.Query
 import javax.inject.Singleton
 
 @Singleton
 interface NetworkService {
 
-    @Headers("X-Api-Key: $API_KEY")
     @GET("top-headlines")
     suspend fun getTopHeadlines(@Query("country") country: String = "us",
                                 @Query("sources") sources: String = "",
                                 @Query("language") language: String = "en",
                                 ): TopHeadlinesResponse
 
-    @Headers("X-Api-Key: $API_KEY")
     @GET("sources")
     suspend fun getNewsSources(): SourcesResponse
     
-    @Headers("X-Api-Key: $API_KEY")
     @GET("everything")
     suspend fun searchNews(
         @Query("q") query: String,
--- app/src/main/java/com/gauravbajaj/newsapp/di/module/ApplicationModule.kt
+++ app/src/main/java/com/gauravbajaj/newsapp/di/module/ApplicationModule.kt
@@ -1,18 +1,27 @@
 package com.gauravbajaj.newsapp.di.module
 
 import android.content.Context
+
 import com.gauravbajaj.newsapp.NewsApplication
 import com.gauravbajaj.newsapp.data.api.NetworkService
 import com.gauravbajaj.newsapp.di.ApplicationContext
 import com.gauravbajaj.newsapp.di.BaseUrl
+import com.gauravbajaj.newsapp.utils.AppConstant.API_KEY
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import dagger.Module
 import dagger.Provides
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
-class ApplicationModule(private val application: NewsApplication) {
+class ApplicationModule(
+    private val application: NewsApplication
+) {
     @ApplicationContext
     @Provides
     fun provideContext(): Context {
@@ -22,6 +31,10 @@ class ApplicationModule(private val application: NewsApplication) {
     @BaseUrl
     @Provides
     fun provideBaseUrl(): String = "https://newsapi.org/v2/"
+    
+    @Provides
+    @Singleton
+    fun provideGson(): Gson = GsonBuilder().create()
 
     @Provides
     @Singleton
@@ -29,14 +42,51 @@ class ApplicationModule(private val application: NewsApplication) {
 
     @Provides
     @Singleton
-    fun provideNetworkService(
+    fun provideApiKeyInterceptor(): Interceptor = Interceptor { chain ->
+        val original = chain.request()
+        val originalHttpUrl = original.url
+
+        val url = originalHttpUrl.newBuilder()
+            .addQueryParameter("apiKey", API_KEY)
+            .build()
+
+        val requestBuilder = original.newBuilder()
+            .url(url)
+
+        val request = requestBuilder.build()
+        chain.proceed(request)
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(
+        apiKeyInterceptor: Interceptor
+    ): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(apiKeyInterceptor)
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(
         @BaseUrl baseUrl: String,
-        gsonConverterFactory: GsonConverterFactory
-    ): NetworkService {
+        gsonConverterFactory: GsonConverterFactory,
+        okHttpClient: OkHttpClient
+    ): Retrofit {
         return Retrofit.Builder()
             .baseUrl(baseUrl)
+            .client(okHttpClient)
             .addConverterFactory(gsonConverterFactory)
             .build()
-            .create(NetworkService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNetworkService(retrofit: Retrofit): NetworkService {
+        return retrofit.create(NetworkService::class.java)
     }
 }
